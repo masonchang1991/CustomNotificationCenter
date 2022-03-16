dd98aff1c0e0aaa4a54c637b185f7a06b28b839b
Mason <masonchang1991@gmail.com>
2022-03-16 15:19:26 +0800

    Initial Commit

---
+++ CustomNotificationCenter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomNotificationCenter
+//
+//  Created by Mason on 2022/3/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
